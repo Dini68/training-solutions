6b578ed0855291b3aaebfa75a6a7219bffad9aeb
Dini68 <kdini68@gmail.com>
2020-11-09 19:49:52 +0100

    Literálok összefűzés osztás nagy szám.

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,33 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("1" + 2);
+        System.out.println(1 + "2");
+        System.out.println("1" + "2");
+
+        double quotient;
+
+        quotient = 3 / 4;
+
+        System.out.println(quotient);
+
+        quotient = 3.0 / 4;
+
+        System.out.println(quotient);
+
+        quotient = 3D / 4;
+
+        System.out.println(quotient);
+
+        quotient = (double)3 / 4;
+
+        System.out.println(quotient);
+
+        long big = 3_244_444_444L;
+
+        System.out.println(big);
+    }
+}
