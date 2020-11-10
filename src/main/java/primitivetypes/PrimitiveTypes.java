000d763ae63db3f3008141efa385d18308131d41
Dini68 <kdini68@gmail.com>
2020-11-10 07:00:13 +0100

    for ciklussal, mert a while-t még nem vettük.

--- src/main/java/primitivetypes/PrimitiveTypes.java
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -7,7 +7,13 @@ public class PrimitiveTypes {
         String s = "";
         int m = 0;
 
-        while (n > 0) {
+        for (int j=0; j<32; j++) {
+            m = n % 2;
+            n = n / 2;
+            s = m + s;
+        }
+
+/*        while (n > 0) {
             m = n % 2;
             n = n / 2;
             s = m + s;
@@ -16,7 +22,7 @@ public class PrimitiveTypes {
         while (s.length()<32) {
             s = 0 + s;
         }
-
+*/
         return s;
     }
 }
