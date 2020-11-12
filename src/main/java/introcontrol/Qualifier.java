9b94b676e0bbf2a0c36feabb33a8104262eef409
Dini68 <kdini68@gmail.com>
2020-11-12 18:37:33 +0100

    minősítő kész

---
+++ src/main/java/introcontrol/Qualifier.java
@@ -0,0 +1,22 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class Qualifier {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        if (number > 100) {
+            System.out.println("nagyobb, mint száz");
+        }
+        else {
+            System.out.println("száz, vagy kisebb");
+        }
+    }
+}
