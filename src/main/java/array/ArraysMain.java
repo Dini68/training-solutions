4c0f1f00cb8b529116c9894d45ac7475ea597b38
Dini68 <kdini68@gmail.com>
2020-11-13 23:35:56 +0100

    tömbök

---
+++ src/main/java/array/ArraysMain.java
@@ -0,0 +1,11 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArraysMain {
+
+    public String numberOfDaysAsString() {
+        int [] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
+}
