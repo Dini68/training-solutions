75b4762801f7785ae23addab92a191b109f85c60
Dini68 <kdini68@gmail.com>
2020-11-17 23:34:31 +0100

    A kör kerülete és területe

---
+++ src/main/java/finalmodifier/CircleCalculator.java
@@ -0,0 +1,22 @@
+package finalmodifier;
+
+public class CircleCalculator {
+
+    private static final double PI = 3.14;
+
+    public double calculatePerimeter(double r) {
+        return 2 * r * PI;
+    }
+
+    public double calculateArea(double r) {
+        return r * r * PI;
+    }
+
+    public static void main(String[] args) {
+        CircleCalculator circleCalculator = new CircleCalculator();
+
+        System.out.println("A kör kerülete: " + circleCalculator.calculatePerimeter(10));
+        System.out.println("A kör területe: " + circleCalculator.calculateArea(10));
+
+    }
+}
