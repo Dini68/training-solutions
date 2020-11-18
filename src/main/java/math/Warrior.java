810daee12f372b98ed26a1bd31c6300a180bdfd2
Dini68 <kdini68@gmail.com>
2020-11-18 23:49:03 +0100

    jó kis játék kezdet

---
+++ src/main/java/math/Warrior.java
@@ -0,0 +1,39 @@
+package math;
+
+public class Warrior {
+
+    private String name;
+
+    private int stamina;
+
+    private double skill;
+
+    public Warrior (String name, Point point) {
+
+    }
+
+    public Point getPosition() {
+        Point p = new Point(0, 0);
+        return p;
+    }
+
+    public void move(Warrior warrior) {
+
+    }
+
+    public void attack(Warrior warrior) {
+
+    }
+
+    public double distance(Warrior warrior) {
+        return 1;
+    }
+
+    public boolean isAlive() {
+        return false;
+    }
+
+    public String toString() {
+        return " ";
+    }
+}
