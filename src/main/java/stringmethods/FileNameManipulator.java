64e023436f1548d2c079f35a0a8cc9b8effc3c57
Dini68 <kdini68@gmail.com>
2020-11-20 20:28:29 +0100

    string műveletek manipulálás csere is

--- src/main/java/stringmethods/FileNameManipulator.java
+++ src/main/java/stringmethods/FileNameManipulator.java
@@ -17,20 +17,23 @@ public class FileNameManipulator {
     public boolean compareFilesByName(String searchedFileName, String actualFileName) {
         return searchedFileName.substring(0, searchedFileName.indexOf(".")).
                 equals(actualFileName.substring(0, actualFileName.indexOf(".")));
-
     }
 
     public String changeExtensionToLowerCase(String fileName) {
         return fileName.substring(0, fileName.indexOf('.') + 1) + findFileExtension(fileName).toLowerCase();
+    }
 
+    public String replaceStringPart(String fileName, String present, String target) {
+        return fileName.replace(present, target);
     }
 
     public static void main(String[] args) {
         FileNameManipulator fileNameManipulator = new FileNameManipulator();
-        System.out.println(fileNameManipulator.findLastCharacter("Kacsaszárny"));
-        System.out.println(fileNameManipulator.findFileExtension("Kacsa.txt"));
-        System.out.println(fileNameManipulator.identifyFilesByExtension("txt", "Kacsa.txt"));
-        System.out.println(fileNameManipulator.compareFilesByName("Kacsa.txt","Kacsa.png"));
-        System.out.println(fileNameManipulator.changeExtensionToLowerCase("Kacsa.PNG"));
+        System.out.println(fileNameManipulator.findLastCharacter("kacsaszárny"));
+        System.out.println(fileNameManipulator.findFileExtension("kacsa.txt"));
+        System.out.println(fileNameManipulator.identifyFilesByExtension("txt", "kacsa.txt"));
+        System.out.println(fileNameManipulator.compareFilesByName("kacsa.txt","kacsa.png"));
+        System.out.println(fileNameManipulator.changeExtensionToLowerCase("kacsa.PNG"));
+        System.out.println(fileNameManipulator.replaceStringPart("kacsa.txt", "sa", "sanyak"));
     }
 }
