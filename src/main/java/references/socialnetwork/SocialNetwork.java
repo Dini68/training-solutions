2bde90d924e43c47a2624c4a3aaf6bf2ba97d80d
Dini68 <kdini68@gmail.com>
2020-11-22 09:35:09 +0100

    SocialNetwork segítséggel elkészítve.

--- src/main/java/references/socialnetwork/SocialNetwork.java
+++ src/main/java/references/socialnetwork/SocialNetwork.java
@@ -12,5 +12,33 @@ public class SocialNetwork {
         members.add(newMember);
     }
 
+    public void connect(String name, String otherName){
+        Member first = findByName(name);
+        first.connectMember(findByName(otherName));
+    }
 
+    private Member findByName (String name){
+        for (Member member: members
+             ) {
+            if (member.getName().equals(name)){
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public List<String> bidirectionalConnections() {
+        List<String> connections = new ArrayList<>();
+        for (Member member: members
+             ) {
+            for (Member member2: member.getConnections()
+                 ) {
+                if (member2.getConnections().contains(member)) {
+                    connections.add(member.getName() + " - " + member2.getName());
+
+                }
+            }
+        }
+        return connections;
+    }
 }
