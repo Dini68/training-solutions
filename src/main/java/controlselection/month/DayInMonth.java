a812d5c14467d3d4d127a18cb4e833bd65663b46
Dini68 <kdini68@gmail.com>
2020-11-23 09:04:26 +0100

    hány napos a megadott hónap? kész.

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,69 @@
+package controlselection.month;
+
+public class DayInMonth {
+
+    public int dayOfMonthNumber (int year, String month){
+        int dayNumber = 0;
+        switch (month.toLowerCase()){
+            case "január" :
+                dayNumber = 31;
+                break;
+            case "február" :
+                dayNumber = 28;
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                    dayNumber = 29;
+                }
+                break;
+            case "március" :
+                dayNumber = 31;
+                break;
+            case "április" :
+                dayNumber = 30;
+                break;
+            case "május" :
+                dayNumber = 31;
+                break;
+            case "június" :
+                dayNumber = 30;
+                break;
+            case "július" :
+                dayNumber = 31;
+                break;
+            case "augusztus" :
+                dayNumber = 31;
+                break;
+            case "szeptember" :
+                dayNumber = 30;
+                break;
+            case "október" :
+                dayNumber = 31;
+                break;
+            case "november" :
+                dayNumber = 30;
+                break;
+            case "december" :
+                dayNumber = 31;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+        }
+        System.out.println(year + " " + month + ": " + dayNumber + " napos hónap");
+        return dayNumber;
+    }
+
+    public static void main(String[] args) {
+
+        DayInMonth dayInMonth = new DayInMonth();
+
+        dayInMonth.dayOfMonthNumber(1900, "február");
+        dayInMonth.dayOfMonthNumber(2020, "Február");
+        dayInMonth.dayOfMonthNumber(2000, "február");
+        dayInMonth.dayOfMonthNumber(2021, "FEBRUÁR");
+
+        dayInMonth.dayOfMonthNumber(1900, "április");
+        dayInMonth.dayOfMonthNumber(2020, "December");
+        dayInMonth.dayOfMonthNumber(2000, "Június");
+        dayInMonth.dayOfMonthNumber(2021, "Januá");
+    }
+
+}
