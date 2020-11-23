dd0cf5b733e6df23afe6c906d41e5c08174ec1da
Dini68 <kdini68@gmail.com>
2020-11-23 10:04:07 +0100

    köszönés napszak szerint tesztelve

--- src/main/java/controlselection/greetings/Greetings.java
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -2,24 +2,24 @@ package controlselection.greetings;
 
 public class Greetings {
 
-    public String Greet( int hour, int minute){
+    public String greet( int hour, int minute){
 
         String s = "";
 
         if  ((hour * 60 + minute <= 300) || (hour * 60 + minute > 1200)) {
-            s = "Jó éjt!";
+            s = "jó éjt";
         }
 
         if  ((hour * 60 + minute > 300) && (hour * 60 + minute <= 540)) {
-            s = "Jó reggelt!";
+            s = "jó reggelt";
         }
 
         if  ((hour * 60 + minute > 540) && (hour * 60 + minute <= 1110)) {
-            s = "Jó napot!";
+            s = "jó napot";
         }
 
         if  ((hour * 60 + minute > 1110) && (hour * 60 + minute <= 1200)) {
-            s = "Jó estét!";
+            s = "jó estét";
         }
 
         return s;
@@ -29,6 +29,6 @@ public class Greetings {
     public static void main(String[] args) {
 
         Greetings greetings = new Greetings();
-        System.out.println(greetings.Greet(20,30));
+        System.out.println(greetings.greet(20,30));
     }
 }
