d724cc8e64351236300afec496556ef0b7da58a4
Dini68 <kdini68@gmail.com>
2020-11-24 15:52:27 +0100

    angol magánhangzók cseréje * ra

---
+++ src/main/java/week05d02/ChangeLetter.java
@@ -0,0 +1,20 @@
+package week05d02;
+
+public class ChangeLetter {
+
+    public String changeVowels(String text){
+        String modifiedText = text;
+        String englishVowels = "aeiouAEIOU";
+        for (int i = 0; i < englishVowels.length(); i++) {
+            modifiedText = modifiedText.replace(englishVowels.charAt(i), '*');
+        }
+        return modifiedText;
+    }
+
+    public static void main(String[] args) {
+        ChangeLetter changeLetter = new ChangeLetter();
+
+        String text = "A szöveg nagyon egyszerű mintákra való illeszkedését vizsgálja";
+        System.out.println(changeLetter.changeVowels(text));
+    }
+}
