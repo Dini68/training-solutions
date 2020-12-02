c4a600da6e86629d1ad434e073c9c2d73a6446d6
Dini68 <kdini68@gmail.com>
2020-12-02 18:06:30 +0100

    palindroma tesztelve null- ra is.

--- src/main/java/stringbuilder/Palindroma.java
+++ src/main/java/stringbuilder/Palindroma.java
@@ -3,6 +3,9 @@ package stringbuilder;
 public class Palindroma {
 
     public boolean isPalindrom (String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("Null a paraméter!");
+        }
         StringBuilder sb = new StringBuilder();
         for (int i = text.length() - 1; i >= 0 ; i--) {
             sb.append(text.charAt(i));
--- src/test/java/stringbuilder/PalindromaTest.java
+++ src/test/java/stringbuilder/PalindromaTest.java
@@ -1,26 +1,30 @@
 package stringbuilder;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PalindromaTest {
+    Palindroma palindroma = new Palindroma();
 
     @Test
     public void textIsPalindromTest() {
-        Palindroma palindroma = new Palindroma();
         assertEquals(true, palindroma.isPalindrom("gézakékazég"));
-        assertEquals(true, palindroma.isPalindrom("indulagörögaludni"));
-        assertEquals(true, palindroma.isPalindrom("Indulagörögaludni"));
     }
     @Test
     public void textWithSpaceIsPalindromTest() {
-        Palindroma palindroma = new Palindroma();
         assertEquals(false, palindroma.isPalindrom("géza kék az ég"));
     }
     @Test
-    public void textwithUpperIsPalindromTest() {
-        Palindroma palindroma = new Palindroma();
+    public void textWithUpperIsPalindromTest() {
         assertEquals(true, palindroma.isPalindrom("Indulagörögaludni"));
     }
+    @Test
+    public void textWithNullIsPalindromTest() throws IllegalArgumentException {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> palindroma.isPalindrom(null));
+        Assertions.assertEquals("Null a paraméter!", ex.getMessage());
+    }
+
 }
