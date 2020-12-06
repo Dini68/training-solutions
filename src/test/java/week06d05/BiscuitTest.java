e48e46ee6e40fc287e01957294493158f759e53f
Dini68 <kdini68@gmail.com>
2020-12-06 20:28:58 +0100

    keksz kész, tesztelés nélkül

---
+++ src/test/java/week06d05/BiscuitTest.java
@@ -0,0 +1,13 @@
+package week06d05;
+
+import org.junit.jupiter.api.Test;
+
+public class BiscuitTest {
+
+    Biscuit biscuit = new Biscuit(BiscuitType.CHOCOLATE,200);
+    @Test
+    public void biscuitOfTest() {
+//        biscuit.of
+
+    }
+}
