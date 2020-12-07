8f450d138fda13fa94934339d3c2a3a690f98a38
Dini68 <kdini68@gmail.com>
2020-12-07 16:52:27 +0100

    Biscuit tesztelve

--- src/test/java/week06d05/BiscuitTest.java
+++ src/test/java/week06d05/BiscuitTest.java
@@ -2,12 +2,13 @@ package week06d05;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BiscuitTest {
 
-    Biscuit biscuit = new Biscuit(BiscuitType.CHOCOLATE,200);
     @Test
     public void biscuitOfTest() {
-//        biscuit.of
-
+        assertEquals(200, Biscuit.of(BiscuitType.OREO,200).getGramAmount());
+        assertEquals(BiscuitType.OREO, Biscuit.of(BiscuitType.OREO,200).getType());
     }
 }
