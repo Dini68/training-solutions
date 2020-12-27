c5491ed230324fabaeb15fbbe3a9d08de1299e39
Dini68 <kdini68@gmail.com>
2020-12-27 17:09:50 +0100

    referencia progi kozmetikázva.

--- src/main/java/references/parameters/ReferencesMain.java
+++ src/main/java/references/parameters/ReferencesMain.java
@@ -15,24 +15,26 @@ public class ReferencesMain {
         person1 = newPerson;
         person2 = person1;
         person2.setName("D");
-        System.out.println(Arrays.asList(person1.getName()));
-        System.out.println(Arrays.asList(person2.getName()));
+        System.out.println("person1: " + person1 + " " + person1.toString());
+        System.out.println("person2: " + person2 + " " + person2.toString());
+        System.out.println("A két változó referenciája ugyanarra az objektumra mutat.\n");
         // a refernciák ugyanarra az objektumra mutatnak
 
         int a = 24;
         int b = a;
         b +=1;
 
-        System.out.println(a + " " + b);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("A prímitív típus esetén nincs hatás a másikra.\n");
 
-        Person new2Person = new Person("K",52);
+        Person new2Person = new Person("H",9);
         person1 = new2Person;
 
-        System.out.println(person1);
-        System.out.println(new2Person);
-        System.out.println(Arrays.asList(person1));
-        System.out.println(Arrays.asList(new2Person));
-        // ua a referencia;
+        System.out.println("person1: " + person1 + " " + person1.toString());
+        System.out.println("person2: " + person2 + " " + person2.toString());
+        System.out.println("A két változó referenciája különböző objektumokra mutat.\n");
+        // a refernciák ugyanarra az objektumra mutatnak
 
     }
 }
