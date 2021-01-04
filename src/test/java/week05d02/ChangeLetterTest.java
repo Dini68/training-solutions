41e88bc0b4203dbdfbed7ee6bd941c5c69708ce4
Dini68 <kdini68@gmail.com>
2021-01-04 19:57:55 +0100

    ChangeLetter kozmetikázva.

--- src/test/java/week05d02/ChangeLetterTest.java
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -2,18 +2,24 @@ package week05d02;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertEquals;
 
 public class ChangeLetterTest {
 
     @Test
-    public void test() {
+    public void testChangeVowels() {
         assertEquals("* pr*gr*m*zás *l*pj**t m*gért*n* f*nt*s lépés.",
                 new ChangeLetter().changeVowels("A programozás alapjait megérteni fontos lépés."));
         assertEquals("*z * m*nd*t *nn*ntől N*GY B*TŰS. * M*L V*D* jó cs*p*t.",
                 new ChangeLetter().changeVowels("Ez a mondat innentől NAGY BETŰS. A MOL VIDI jó csapat."));
         assertEquals("*z *z *t*lsó t*sztm*nd*t. T*l*f*nszám: 0124-56-78",
                 new ChangeLetter().changeVowels("Ez az utolsó tesztmondat. Telefonszám: 0124-56-78"));
+    }
+
+    @Test
+    public void test() {
+        assertThrows(IllegalArgumentException.class, () -> new ChangeLetter().changeVowels(null));
+        assertThrows(IllegalArgumentException.class, () -> new ChangeLetter().changeVowels("   "));
     }
 }
