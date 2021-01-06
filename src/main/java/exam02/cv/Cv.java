394804ef15959eb5edb67a499181805c93f21aea
Dini68 <kdini68@gmail.com>
2021-01-06 18:26:15 +0100

    Cv kész.

--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -1,7 +1,34 @@
 package exam02.cv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cv {
 
     private String name;
 
+    private List<Skill> skills = new ArrayList<>();
+
+    public Cv(String name, List<Skill> skills) {
+        this.name = name;
+        this.skills = skills;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int findSkillLevelByName(String skillName) {
+        if (skillName == null || skillName.isBlank()) {
+            throw new IllegalArgumentException("Nincs név.");
+        }
+        int skillLevel = 0;
+        for (Skill sk: skills) {
+            if (sk.getName().equals(skillName)) {
+                skillLevel = sk.getLevel();
+            }
+        }
+
+        return skillLevel;
+    }
 }
