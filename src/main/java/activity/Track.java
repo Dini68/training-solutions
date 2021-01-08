a6e5da742ba90fd5fe8f79f9a72a29c563c2c8de
Dini68 <kdini68@gmail.com>
2021-01-08 22:04:26 +0100

    Activity projekt 4. rész

--- src/main/java/activity/Track.java
+++ src/main/java/activity/Track.java
@@ -6,4 +6,36 @@ import java.util.List;
 public class Track {
 
     private List<TrackPoint> trackPoints = new ArrayList<>();
+
+    public void addTrackPoint(TrackPoint trackPoint) {
+        trackPoints.add(trackPoint);
+    }
+
+    public Coordinate findMaximumCoordinate () {
+        return new Coordinate(1,1);
+    }
+
+    public Coordinate findMinimumCoordinate () {
+        return new Coordinate(1,1);
+    }
+
+    public double getDistance() {
+        return 1;
+    }
+
+    public double getFullDecrease() {
+        return 1;
+    }
+
+    public double getFullElevation() {
+        return 1;
+    }
+
+    public double GetRectangleArea() {
+        return 1;
+    }
+
+    public List<TrackPoint> getTrackPoints() {
+        return trackPoints;
+    }
 }
--- src/main/java/activity/TrackPoint.java
+++ src/main/java/activity/TrackPoint.java
@@ -16,10 +16,32 @@ public class TrackPoint {
     }
 
     public double getDistanceFrom(TrackPoint trackPoint) {
-        double x = this.coordinate.getLatitude() - trackPoint.getCoordinate().getLatitude();
-        double y = this.coordinate.getLongitude() - trackPoint.getCoordinate().getLongitude();
-        double z = this.elevation - trackPoint.getElevation();
-        return Math.sqrt(x * x + y * y + z * z);
+        final int R = 6371; // Radius of the earth
+
+        double lat1 = this.coordinate.getLatitude();
+        double lat2 = trackPoint.coordinate.getLatitude();
+        double latDistance = Math.toRadians(lat2 - lat1);
+
+        double lon1 = this.coordinate.getLongitude();
+        double lon2 = trackPoint.coordinate.getLongitude();
+        double lonDistance = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double distance = R * c * 1000; // convert to meters
+
+        double el1 = this.elevation;
+        double el2 = trackPoint.getElevation();
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
     }
 
     public double getElevation() {
