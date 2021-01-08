365ba484b0c344933376e8d288de5923b7936a50
Dini68 <kdini68@gmail.com>
2021-01-08 19:17:27 +0100

    Activity projekt 3. rész

--- src/main/java/activity/Coordinate.java
+++ src/main/java/activity/Coordinate.java
@@ -2,11 +2,17 @@ package activity;
 
 public class Coordinate {
 
-    private double latitude;
+    private final double latitude;
 
-    private double longitude;
+    private final double longitude;
 
     public Coordinate(double latitude, double longitude) {
+        if (latitude < -90 || latitude > 90) {
+            throw new IllegalArgumentException("Rossz adat.");
+        }
+        if (longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException("Rossz adat.");
+        }
         this.latitude = latitude;
         this.longitude = longitude;
     }
