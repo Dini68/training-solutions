697f442debeedf7354d051a351e4d6e6aa707c39
Dini68 <kdini68@gmail.com>
2021-01-11 18:02:16 +0100

    DivisorFinder kész negatív számokra is, tesztelve, abs nélkül.

--- src/main/java/week11d01/DivisorFinder.java
+++ src/main/java/week11d01/DivisorFinder.java
@@ -5,8 +5,8 @@ public class DivisorFinder {
     public int findDivisors(int n) {
         int numberOfDivisors = 0;
         int digit = 0;
-        int number = Math.abs(n);
-        while (number > 0) {
+        int number = n;
+        while (number != 0) {
             digit = number % 10;
             if (digit != 0 && n % digit == 0) {
                 numberOfDivisors++;
