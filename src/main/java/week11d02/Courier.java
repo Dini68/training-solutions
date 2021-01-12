fd9e014c35ae4c0a898fc5c1926b74f5c283c92b
Dini68 <kdini68@gmail.com>
2021-01-12 20:25:46 +0100

    Ride javítva, tesztelve.

--- src/main/java/week11d02/Courier.java
+++ src/main/java/week11d02/Courier.java
@@ -12,14 +12,31 @@ public class Courier {
             throw new IllegalArgumentException("ride cannot be not null");
         }
 
+        if (ride.getDay() < 1 || ride.getDay() > 7)  {
+            throw new IllegalArgumentException("The number of days should be between 1 and 7.");
+        }
+
+        if (ride.getRideNr() < 1)  {
+            throw new IllegalArgumentException("The number of ride must be at least 1.");
+        }
+
         boolean added = false;
 
-        if (rides.isEmpty()) {
+        if (rides.isEmpty() && ride.getRideNr() != 1) {
+            throw new IllegalArgumentException("The number of ride must be 1.");
+        }
+
+        if (rides.isEmpty() && ride.getRideNr() == 1) {
             rides.add(ride);
             added = true;
         }
 
+
         int last = rides.size() - 1;
+
+        if (rides.get(last).getDay() > ride.getDay() && !added) {
+            throw new IllegalArgumentException("The day of number is wrong.");
+        }
 
         if (rides.get(last).getDay() < ride.getDay() && !added) {
             if (ride.getRideNr() == 1) {
@@ -59,12 +76,10 @@ public class Courier {
 
     public static void main(String[] args) {
         Courier cu = new Courier();
-        Ride r1=new Ride(1,1,34.5);
-        Ride r2=new Ride(2,1,34.5);
-        Ride r3=new Ride(3,1,34.5);
+        Ride r1=new Ride(3,1,34.5);
         cu.addRide(r1);
+        Ride r2=new Ride(2,1,34.5);
         cu.addRide(r2);
-        cu.addRide(r3);
         System.out.println(cu.firstFreeDay());
     }
 }
---
+++ src/test/java/week11d02/CourierTest.java
@@ -0,0 +1,85 @@
+package week11d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourierTest {
+
+    @Test
+    void addRideWithNull() {
+        Courier cu = new Courier();
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(null));
+    }
+    @Test
+    void addRideWithErrorDay() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(0,1,34.5);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r1));
+    }
+    @Test
+    void addRideWithErrorDay2() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(8,1,34.5);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r1));
+    }
+    @Test
+    void addRideWithErrorDay3() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(3,1,34.5);
+        cu.addRide(r1);
+        Ride r2=new Ride(2,1,34.5);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r2));
+    }
+    @Test
+    void addRideWithErrorRide() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(1,1,34.5);
+        Ride r2=new Ride(2,1,34.5);
+        Ride r3=new Ride(2,3,34.5);
+        cu.addRide(r1);
+        cu.addRide(r2);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r3));
+    }
+    @Test
+    void addRideWithErrorRide3() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(4,1,34.5);
+        Ride r2=new Ride(4,2,34.5);
+        Ride r3=new Ride(4,1,34.5);
+        cu.addRide(r1);
+        cu.addRide(r2);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r3));
+    }
+    void addRideWithErrorRide2() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(1,1,34.5);
+        Ride r2=new Ride(2,1,34.5);
+        Ride r3=new Ride(2,0,34.5);
+        cu.addRide(r1);
+        cu.addRide(r2);
+        assertThrows(IllegalArgumentException.class, () -> cu.addRide(r3));
+    }
+    @Test
+    void firstFreeDay1() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(1,1,34.5);
+        Ride r2=new Ride(2,1,34.5);
+        Ride r3=new Ride(3,1,34.5);
+        cu.addRide(r1);
+        cu.addRide(r2);
+        cu.addRide(r3);
+        assertEquals(4, cu.firstFreeDay());
+    }
+    @Test
+    void firstFreeDay2() {
+        Courier cu = new Courier();
+        Ride r1=new Ride(2,1,34.5);
+        Ride r2=new Ride(2,2,34.5);
+        Ride r3=new Ride(5,1,34.5);
+        cu.addRide(r1);
+        cu.addRide(r2);
+        cu.addRide(r3);
+        assertEquals(1, cu.firstFreeDay());
+    }
+}
