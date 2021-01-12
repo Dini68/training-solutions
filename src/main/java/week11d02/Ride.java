8ab9919e3ff2e4c32d0acd1d106f4446be735589
Dini68 <kdini68@gmail.com>
2021-01-12 21:49:06 +0100

    Ride javítva, tesztelve.

--- src/main/java/week11d02/Ride.java
+++ src/main/java/week11d02/Ride.java
@@ -6,6 +6,16 @@ public class Ride {
     private double length;
 
     public Ride(int day, int rideNr, double length) {
+        if (day < 1 || day > 7) {
+            throw new IllegalArgumentException("The number of days should be between 1 and 7.");
+        }
+        if (rideNr < 1) {
+            throw new IllegalArgumentException("The number of ride must be at least 1.");
+        }
+        if (!(length > 0)) {
+            throw new IllegalArgumentException("The length is wrong.");
+        }
+
         this.day = day;
         this.rideNr = rideNr;
         this.length = length;
