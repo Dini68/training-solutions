b122305c24334ba9557d122d5868af4dbc2784b5
Dini68 <kdini68@gmail.com>
2021-01-14 20:32:33 +0100

    FileManager kész

---
+++ src/main/java/filemanager/FileManager.java
@@ -0,0 +1,34 @@
+package filemanager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+
+    private List<Human> humans = new ArrayList<>();
+
+    public void readFromFile() {
+        List<String> names = new ArrayList<>();
+        if (Files.isRegularFile(Path.of("humans.txt"))) {
+            try {
+                names = Files.readAllLines(Path.of("humans.txt"));
+            } catch (IOException e) {
+                throw new IllegalArgumentException("", e);
+            }
+        }
+        for (String name: names) {
+            humans.add(new Human(name.substring(0, name.indexOf(" ")),name.substring(name.indexOf(" ") + 1, name.length())));
+        }
+        System.out.println(names);
+        for (Human hu: humans) {
+            System.out.println(hu.getLastName() + " " + hu.getFirstName());
+        }
+    }
+
+    public static void main(String[] args) {
+        new FileManager().readFromFile();
+    }
+}
---
+++ src/main/java/filemanager/Human.java
@@ -0,0 +1,21 @@
+package filemanager;
+
+public class Human {
+
+    private String lastName;
+
+    private String firstName;
+
+    public Human(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
