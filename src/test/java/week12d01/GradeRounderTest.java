c1453ef007b38e3925ff4c3bf53f970063d05186
Dini68 <kdini68@gmail.com>
2021-01-18 18:07:04 +0100

    Grades tesztelés kozmetikázva.

--- src/test/java/week12d01/GradeRounderTest.java
+++ src/test/java/week12d01/GradeRounderTest.java
@@ -11,13 +11,8 @@ class GradeRounderTest {
     @Test
     void roundGradesTest() {
         GradeRounder gradeRounder = new GradeRounder();
-        int [] arr = {13, 68, 72, 42, 38, 84, 99, 43};
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(gradeRounder.roundGrades(arr)));
-        assertEquals(13, gradeRounder.roundGrades(arr)[0]);
-        assertEquals(72, gradeRounder.roundGrades(arr)[2]);
-        assertEquals(38, gradeRounder.roundGrades(arr)[4]);
-        assertEquals(100, gradeRounder.roundGrades(arr)[6]);
-        assertEquals(45, gradeRounder.roundGrades(arr)[7]);
+        int [] arrParam = {13, 68, 72, 42, 38, 84, 99, 43};
+        int [] arrResult = {13, 70, 72, 42, 38, 85, 100, 45};
+        assertArrayEquals(arrResult, gradeRounder.roundGrades(arrParam));
     }
 }
