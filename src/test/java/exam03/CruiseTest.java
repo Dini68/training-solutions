6c67366cbc966a5a7e6a43a3878679deb3f8cb3e
Dini68 <kdini68@gmail.com>
2021-02-15 19:43:54 +0100

    Cruise nagyjából kész, tesztelve. 1 bukó.

--- src/test/java/exam03/CruiseTest.java
+++ src/test/java/exam03/CruiseTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CruiseTest {
 
@@ -72,24 +71,24 @@ public class CruiseTest {
         assertEquals(List.of("Jack Doe", "Jack Smith", "John Doe"), names);
     }
 
-//    @Test
-//    void sumAllBookingsCharged() {
-//        cruise.bookPassenger(new Passenger("Jack Smith", CruiseClass.LUXURY));
-//        cruise.bookPassenger(new Passenger("John Doe", CruiseClass.FIRST));
-//        cruise.bookPassenger(new Passenger("Jack Doe", CruiseClass.SECOND));
-//
-//        double sum = cruise.sumAllBookingsCharged();
-//        assertEquals(300_000 + 180_000 + 100_000, sum, 0.5);
-//    }
-//
-//    @Test
-//    void countPassengerByClass() {
-//        cruise.bookPassenger(new Passenger("Jack Smith", CruiseClass.LUXURY));
-//        cruise.bookPassenger(new Passenger("John Doe", CruiseClass.LUXURY));
-//        cruise.bookPassenger(new Passenger("Jack Doe", CruiseClass.FIRST));
-//
-//        Map<CruiseClass, Integer> result = cruise.countPassengerByClass();
-//        assertEquals(Map.of(CruiseClass.LUXURY, 2 , CruiseClass.FIRST, 1), result);
-//    }
+    @Test
+    void sumAllBookingsCharged() {
+        cruise.bookPassenger(new Passenger("Jack Smith", CruiseClass.LUXURY));
+        cruise.bookPassenger(new Passenger("John Doe", CruiseClass.FIRST));
+        cruise.bookPassenger(new Passenger("Jack Doe", CruiseClass.SECOND));
+
+        double sum = cruise.sumAllBookingsCharged();
+        assertEquals(300_000 + 180_000 + 100_000, sum, 0.5);
+    }
+
+    @Test
+    void countPassengerByClass() {
+        cruise.bookPassenger(new Passenger("Jack Smith", CruiseClass.LUXURY));
+        cruise.bookPassenger(new Passenger("John Doe", CruiseClass.LUXURY));
+        cruise.bookPassenger(new Passenger("Jack Doe", CruiseClass.FIRST));
+
+        Map<CruiseClass, Integer> result = cruise.countPassengerByClass();
+        assertEquals(Map.of(CruiseClass.LUXURY, 2 , CruiseClass.FIRST, 1), result);
+    }
 
 }
