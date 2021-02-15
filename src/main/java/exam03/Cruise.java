adb093e6375b3aa230dfc32429c73fd1201218ba
Dini68 <kdini68@gmail.com>
2021-02-15 19:09:16 +0100

    Cruise folytatom nem adom fel.

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,6 +1,7 @@
 package exam03;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,7 +29,7 @@ public class Cruise {
         return passengers;
     }
 
-    private List<Passenger> passengers;
+    private List<Passenger> passengers = new ArrayList<>();
 
     public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
         this.boat = boat;
@@ -58,6 +59,15 @@ public class Cruise {
 
     public Map<CruiseClass, Integer> countPassengerByClass() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        Boat boat = new Boat("WetDreams", 5);
+
+        Cruise cruise = new Cruise(boat, LocalDate.of(2021, 1, 1), 100_000);
+
+        cruise.bookPassenger(new Passenger("John Doe", CruiseClass.LUXURY));
+
     }
 
 }
