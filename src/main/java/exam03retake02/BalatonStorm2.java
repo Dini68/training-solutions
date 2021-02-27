cd8a07b100388ed0a6b063525b0b0d808f3b6c85
Dini68 <kdini68@gmail.com>
2021-02-27 08:31:40 +0100

    exam03 balatonstorm újragondolva.

---
+++ src/main/java/exam03retake02/BalatonStorm2.java
@@ -0,0 +1,51 @@
+package exam03retake02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class BalatonStorm2 {
+
+    public List<String> getStationsInStorm(BufferedReader reader) {
+        List<String> result = new ArrayList<>();
+        try {
+            String line;
+            String name = "";
+            int level = 0;
+            boolean foundStation = false;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("allomas")) {
+                    name = line.split(":")[1].substring(2, line.split(":")[1].length()-2);
+                    System.out.println(name);
+                    foundStation = true;
+                }
+                if (line.contains("level") && foundStation) {
+                    level = Integer.parseInt(line.split(":")[1].substring(1, line.split(":")[1].length()-1));
+                    System.out.println(level);
+                    foundStation = false;
+                    if (level == 3) {
+                        result.add(name);
+                    }
+                }
+            }
+            Collections.sort(result, Collator.getInstance(new Locale("hu", "HU")));
+            return result;
+        } catch (IOException ioe) {
+            throw new IllegalStateException("can not read fil", ioe);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(BalatonStorm.class.getResourceAsStream("storm.json")))) {
+
+        System.out.println(new BalatonStorm2().getStationsInStorm(reader));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("can not read fil", ioe);
+        }
+    }
+}
