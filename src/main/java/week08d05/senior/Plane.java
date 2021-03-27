0ed29c903dc22cf1c7b750c4d873e3b7d9d54da5
Dini68 <kdini68@gmail.com>
2021-03-28 00:04:37 +0100

    gyakorlás

---
+++ src/main/java/week08d05/senior/Plane.java
@@ -0,0 +1,52 @@
+package week08d05.senior;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Plane {
+
+    public static final char OCEAN_CHARACTER = '0';
+
+    public int longestOcean(String path) {
+        int maxLength = 0;
+        try (FileInputStream fis = new FileInputStream(path)) {
+            maxLength = getMaxLength(fis);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException ioException) {
+            System.out.println("IO fail");
+        }
+        return maxLength;
+    }
+
+    private int getMaxLength(FileInputStream fis) throws IOException {
+        int max = 0;
+        int input;
+        int counter = 0;
+        while ((input = fis.read()) != -1){
+            if (input == OCEAN_CHARACTER){
+                counter++;
+            }
+            else {
+                max = getMax(max, counter);
+                counter = 0;
+            }
+        }
+        return max;
+    }
+
+    private int getMax(int max, int counter) {
+        if (max < counter){
+            max = counter;
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        Plane plane = new Plane();
+        System.out.println(plane.longestOcean("src/main/java/week08d05/senior/map.txt"));
+    }
+}
